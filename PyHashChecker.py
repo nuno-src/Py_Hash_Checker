29b4255083f3e454cfa88c49e580436883f405e6
Nuno <75589883+nunocardoso7@users.noreply.github.com>
2022-01-01 02:50:43 +0000

    Add files via upload

---
+++ PyHashChecker.py
@@ -0,0 +1,27 @@
+
+
+
+h1 = input("Insira a 1º hash: ")
+
+h2 = input("Insira a 2º hash: ")
+
+
+# Hash
+hah_h1 = hash(h1)
+
+hah_h2 = hash(h2)
+
+# print(hah_h1)
+# print(hah_h2)
+
+
+
+if h1 == h2 and hah_h1 == hah_h2:
+    print("\nAs Hash's são Iguais.")
+else:
+    print("\nATENÇÃO as Hash's SÃO DIFERENTES!!!")
+
+
+
+
+
